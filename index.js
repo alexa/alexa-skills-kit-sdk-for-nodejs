a9367a7133427c3025fddba6030430379c875dfb
knowlsie <tomobknowles@gmail.com>
2017-04-05 14:46:00 +0100

    Fix index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var AlexaLambdaHandler = require('./lib/alexa');
+const AlexaLambdaHandler = require('./lib/alexa');
 
 module.exports.handler = AlexaLambdaHandler.LambdaHandler;
 module.exports.CreateStateHandler = AlexaLambdaHandler.CreateStateHandler;
