8c20f06c8f624bb2a96fed2fe99a29aa69fe203b
Sai Kalyan <saikalyan@Sais-MacBook-Pro.local>
2018-06-08 13:17:06 -0400

    Introduced speakPrimitive and speakObject functions

--- ask-sdk-core/lib/response/Debug.js
+++ ask-sdk-core/lib/response/Debug.js
@@ -1,7 +1,7 @@
 var debuggingIsOn;
 var timeBetweenEachDebugStatement;
 
-var toBeRead;
+var textToBeRead;
 
 modules.exports = this;
 function Debug() {
@@ -13,13 +13,42 @@ modules.exports = this;
 function speak(var param) {
 	if(debuggingIsOn = false)
 		return;
-		
-	if(param is primitive)
+	
+	var paramType = typeof param; //Get the type of the parameter
+	var paramIsPrimitive = ( paramType === 'number' || paramType === 'boolean'
+								|| paramType === 'string' ); //want to speak a primitive
+	var paramIsObject = ( paramType === 'object' ); //want to speak an object and its fields
+	
+	if(paramIsPrimitive)
 		speakPrimitive(param);
-	else if(param is object)
+	else if(paramIsObject)
 		speakObject(param);
 	else
 		console.log("you have called the speak method. But the parameter is not" + 
 		"a primitive like a string or an integer, nor is it an object. If you" +
 		"are trying to read the state of your slots, use the speakState method");
+}
+
+modules.exports = this;
+function speakPrimitive(var primitiveValue) {
+	var textSnippet = String(primitiveValue); //Turn the primitive value into a string
+	textToBeRead += textSnippet;
+	textToBeRead += timeDelay();
+	//Maybe add a new line after each one, so the text is easier to display
+}
+
+modules.exports = this;
+function speakObject(var object) {
+	if(object === 'null') {
+		//Don't try and look at a null object's fields. When you see a 'null' object
+		//just say the phrase "null" (treat it as a normal primitive string)
+		speakPrimitive("null");
+	}
+	else {
+		for(each field f) {
+			var fieldAndValue = "The value of field " + f + " is " + String(f.val);
+			textToBeRead += fieldAndValue;
+		}
+		textToBeRead += timeDelay();
+	}
 }
