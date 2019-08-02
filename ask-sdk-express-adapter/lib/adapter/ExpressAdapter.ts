6b2914a994d0145f0db4bc7b28a12b042d7e75f9
Shen Chen <shench@amazon.com>
2019-08-01 17:38:33 -0700

    feat: add express adapter package

---
+++ ask-sdk-express-adapter/lib/adapter/ExpressAdapter.ts
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import { createAskSdkError, Skill } from 'ask-sdk-core';
+import { ResponseEnvelope } from 'ask-sdk-model';
+import { text } from 'body-parser';
+import { RequestHandler } from 'express';
+import { asyncVerifyRequestAndDispatch } from '../util';
+import { SkillRequestSignatureVerifier, TimestampVerifier, Verifier } from '../verifier';
+
+/**
+ * Express adapter class
+ */
+export class ExpressAdapter {
+    protected skill : Skill;
+    protected verifiers : Verifier[];
+
+    /**
+     * Constructor
+     *
+     * @param {Skill} skill ask-sdk-core custom skill instance
+     * @param {boolean} verifySignature boolean flag decide if certificate signature verifier is needed
+     * @param {boolean} verifyTimeStamp boolean flag decide if timestamp verifier is needed
+     * @param {Verifier[]} verifiers Array of user customized Verifier instances
+     */
+    constructor(skill : Skill, verifySignature : boolean = true,
+                verifyTimeStamp : boolean = true, verifiers : Verifier[] = []) {
+        this.skill = skill;
+        this.verifiers = verifiers;
+        if (!this.skill) {
+            throw createAskSdkError(
+                this.constructor.name,
+                'The input skill cannot be empty',
+            );
+        }
+        skill.appendAdditionalUserAgent('ask-express-adapter');
+        if (verifySignature) {
+            verifiers.push(new SkillRequestSignatureVerifier());
+        }
+        if (verifyTimeStamp) {
+            verifiers.push(new TimestampVerifier());
+        }
+    }
+
+    /**
+     * Get pre-defined request handlers
+     *
+     * This function return an arry of pre-defined request handlers
+     * which are supposed to be registered on users' express application, including:
+     * 1: text parser 2: async function to get response envelope after verification, then send result back
+     * Example usage: app.post('/', new ExpressAdapter(skill).getASKRequestHandler());
+     */
+    public getRequestHandlers() : RequestHandler[] {
+        const requestHandlers : RequestHandler[] = [];
+        requestHandlers.push(
+            (req, res, next) => {
+                if (req.body) {
+                    res.statusCode = 500;
+                    res.send('Error in processing request. Do not register any parsers before using the adapter');
+
+                    return;
+                }
+                next();
+            },
+        );
+        requestHandlers.push(text({
+            type: '*/*',
+        }));
+        requestHandlers.push(
+            async(req, res) => {
+                try {
+                    const responseEnvelope : ResponseEnvelope = await asyncVerifyRequestAndDispatch(req.headers, req.body, this.skill, this.verifiers);
+                    res.json(responseEnvelope);
+                } catch (err) {
+                    res.statusCode = err.name === 'AskSdk.Request verification failed Error' ? 400 : 500;
+                    res.send(`${err.name}, ${err.message}`);
+                }
+            },
+        );
+
+        return requestHandlers;
+    }
+
+}
---
+++ ask-sdk-express-adapter/tst/adapter/ExpressAdapter.spec.ts
@@ -0,0 +1,120 @@
+import { createAskSdkError, Skill, SkillBuilders } from 'ask-sdk-core';
+import { ResponseEnvelope } from 'ask-sdk-model';
+import { expect } from 'chai';
+import * as express from 'express';
+import * as sinon from 'sinon';
+import * as request from 'supertest';
+import { ExpressAdapter } from '../../lib/adapter/ExpressAdapter';
+import * as util from '../../lib/util';
+import { SkillRequestSignatureVerifier, TimestampVerifier, Verifier } from '../../lib/verifier';
+import { DataProvider } from '../mocks/DataProvider';
+
+describe('ExpressAdapter tests', () => {
+    const skill : Skill = SkillBuilders.custom().create();
+
+    describe('constructor', () => {
+        it('should throw error when input skill is not a valid custom skill instance', () => {
+            const invalidSkill = null;
+            try {
+                const adapter : ExpressAdapter = new ExpressAdapter(invalidSkill);
+            } catch (err) {
+                expect(err.name).equal('AskSdk.ExpressAdapter Error');
+                expect(err.message).equal('The input skill cannot be empty');
+
+                return;
+            }
+            throw new Error('should have thrown an error!');
+        });
+
+        it('should not have any verifier when verifySignature and verifyTimeStamp are set to false', () => {
+            try {
+                const verifiers : Verifier[] = [];
+                const adapter : ExpressAdapter = new ExpressAdapter(skill, false, false, verifiers);
+                const propertyKey : string = 'verifiers';
+                expect(adapter[propertyKey]).deep.equal([]);
+            } catch (err) {
+                expect.fail('should not throw error');
+            }
+        });
+
+        it('should defaultly add SkillRequestSignatureVerifier and TimestampVerifier', () => {
+            try {
+                const adapter : ExpressAdapter = new ExpressAdapter(skill);
+                const fakeTimeStampVerifier = new TimestampVerifier();
+                const fakeSignatureVerifier = new SkillRequestSignatureVerifier();
+                const expectedVerifiers : Verifier[] = [fakeSignatureVerifier, fakeTimeStampVerifier];
+                const propertyKey : string = 'verifiers';
+                expect(adapter[propertyKey]).deep.equal(expectedVerifiers);
+            } catch (err) {
+                expect.fail('should not throw error');
+            }
+        });
+    });
+
+    describe('function getASKRequestHandlers', () => {
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('should return 400 when error is verification error', (done) => {
+            const adapter : ExpressAdapter = new ExpressAdapter(skill);
+            const app : express.Application = express();
+            app.post('/', adapter.getRequestHandlers());
+            sinon.stub(util, 'asyncVerifyRequestAndDispatch').callsFake(() => {
+                throw createAskSdkError('Request verification failed', 'unknownError');
+            });
+            // skip printing error message in console
+            sinon.stub(console, 'error');
+            request(app)
+                .post('/')
+                .expect('Content-Type', 'text/html; charset=utf-8')
+                .expect(400, done);
+        });
+
+        it('should return 500 when error is thrown from request dispatch', (done) => {
+            const adapter : ExpressAdapter = new ExpressAdapter(skill);
+            const app : express.Application = express();
+            app.post('/', adapter.getRequestHandlers());
+            sinon.stub(util, 'asyncVerifyRequestAndDispatch').callsFake(() => {
+                throw createAskSdkError('Skill dispatch failed', 'unknownError');
+            });
+            // skip printing error message in console
+            sinon.stub(console, 'error');
+            request(app)
+                .post('/')
+                .expect('Content-Type', 'text/html; charset=utf-8')
+                .expect(500, done);
+        });
+
+        it('should return valid responseEnvelope when skill is invoked correctly', (done) => {
+            const adapter : ExpressAdapter = new ExpressAdapter(skill);
+            const app : express.Application = express();
+            const responseEnvelope : ResponseEnvelope = DataProvider.responseEnvelope();
+            app.post('/', adapter.getRequestHandlers());
+            sinon.stub(util, 'asyncVerifyRequestAndDispatch').callsFake(() => {
+                return Promise.resolve(responseEnvelope);
+            });
+            request(app)
+                .post('/')
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(responseEnvelope)
+                .expect(200, done);
+        });
+
+        it('should shrow error when request already parsed', (done) => {
+            const adapter : ExpressAdapter = new ExpressAdapter(skill);
+            const app : express.Application = express();
+            const responseEnvelope : ResponseEnvelope = DataProvider.responseEnvelope();
+            app.use(express.json());
+            app.post('/', adapter.getRequestHandlers());
+            sinon.stub(util, 'asyncVerifyRequestAndDispatch').callsFake(() => {
+                return Promise.resolve(responseEnvelope);
+            });
+            request(app)
+                .post('/')
+                .expect('Error in processing request. Do not register any parsers before using the adapter')
+                .expect(500, done);
+        });
+    });
+});
