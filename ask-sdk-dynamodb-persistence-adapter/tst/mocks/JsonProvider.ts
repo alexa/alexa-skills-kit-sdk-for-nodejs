59002f423a13fe3d59e7882d013bde40501d3f79
prashanth bheemagani <prashanthreddy.bheemagani@gmail.com>
2020-01-07 10:51:02 -0800

    feat: Add Person ID partition key generator (#593)

--- ask-sdk-dynamodb-persistence-adapter/tst/mocks/JsonProvider.ts
+++ ask-sdk-dynamodb-persistence-adapter/tst/mocks/JsonProvider.ts
@@ -36,6 +36,9 @@ export const JsonProvider = {
                     user : {
                         userId : null,
                     },
+                    person : {
+                        personId : null,
+                    },
                 },
             },
             request: null,
