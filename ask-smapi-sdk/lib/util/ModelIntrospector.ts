cb57328855b6095cfae5ed28cfc1f06a097e60be
ShenChen-Amazon <45435608+ShenChen-Amazon@users.noreply.github.com>
2020-03-04 12:49:42 -0800

    feat: Implement model introspector (#612)

---
+++ ask-smapi-sdk/lib/util/ModelIntrospector.ts
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import { nodes, PathComponent } from 'jsonpath';
+
+/**
+ * Introspects on the Skill Managament API (SMAPI) model.
+ */
+export class ModelIntrospector {
+    private operations : Map<string, ApiOperation>;
+    private definitions : Map<string, TypeDefinition>;
+    private modelJson : any;
+    private customizationProcessor : CustomizationProcessor;
+
+    constructor(modelJson? : any, customizationProcessor? : CustomizationProcessor) {
+        if (!modelJson) {
+            const modelContent : string = require('ask-smapi-model/spec.json');
+            this.modelJson = modelContent;
+        } else {
+            this.modelJson = modelJson;
+        }
+        this.customizationProcessor = customizationProcessor;
+        this.processModel();
+    }
+
+    /**
+     * Returns the operation definition for a given Skill Managament API (SMAPI) operation.
+     * @param operationName operation name
+     */
+    public getOperationDefinition(operationName : string) : ApiOperation {
+        return this.operations.get(operationName);
+    }
+
+    /**
+     * Returns a Map of all Skill Managament API (SMAPI) operations
+     */
+    public getOperations() : Map<string, ApiOperation> {
+        return this.operations;
+    }
+
+    private processModel() : void {
+        const modelDefinitions : Array<{ path : PathComponent[]; value : any; }> = nodes(this.modelJson, '$["definitions"][*]');
+        this.definitions = new Map();
+        for (const definition of modelDefinitions) {
+            this.definitions.set(definition.path[definition.path.length - 1].toString(), definition.value);
+        }
+        const operationDefinitions : Array<{ path : PathComponent[]; value : any; }> = nodes(this.modelJson, '$["paths"][*][*]');
+        this.operations = new Map();
+        for (const operation of operationDefinitions) {
+            const operationDefinition = operation.value;
+            const apiOperationName = operationDefinition['x-operation-name'];
+            if (apiOperationName) {
+                const apiVersion = parseInt(apiOperationName.substring(apiOperationName.length - 1), 10);
+                const apiOperation : ApiOperation = {
+                    apiOperationName,
+                    apiVersion,
+                    description: operationDefinition.description,
+                    params: operationDefinition.parameters,
+                    customizationMetadata: {},
+                };
+                const processedOperationName = this.customizationProcessor ? this.customizationProcessor.processOperationName(apiOperationName) : apiOperationName;
+                if (this.customizationProcessor) {
+                    this.customizationProcessor.processOperation(processedOperationName, apiOperation, this.definitions);
+                    for (const param of apiOperation.params) {
+                        this.customizationProcessor.processParameter(param, apiOperation, this.definitions);
+                    }
+                }
+                if (!this.operations.has(processedOperationName) || apiVersion > this.operations.get(processedOperationName).apiVersion) {
+                    this.operations.set(processedOperationName, apiOperation);
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Customization processors are executed while processing a specification and can apply custom logic.
+ */
+export interface CustomizationProcessor {
+    /**
+     * Processes an API operation name
+     * @param operationName operation name
+     */
+    processOperationName(operationName : string) : string;
+
+    /**
+     * Processes an API operation
+     * @param operationName processed operation name
+     * @param operation operation definition
+     * @param definitions map of modeled API definitions
+     */
+    processOperation(operationName : string, operation : ApiOperation, definitions : Map<string, TypeDefinition>) : void;
+
+    /**
+     * Processes an API parameter
+     * @param parameter parameter definition
+     * @param parentOperation operation this parameter belongs to
+     * @param definitions map of modeled API definitions
+     */
+    processParameter(parameter : ApiParameter, parentOperation : ApiOperation, definitions : Map<string, TypeDefinition>) : void;
+}
+
+/**
+ * Represents an API operation.
+ */
+export interface ApiOperation {
+    apiOperationName : string;
+    apiVersion : number;
+    description : string;
+    params : ApiParameter[];
+    customizationMetadata : { [key : string] : any };
+}
+
+/**
+ * Represents an API type definition.
+ */
+export interface TypeDefinition {
+    type : string;
+    description : string;
+    required : boolean;
+    enum : string[];
+    items : {};
+    properties : { [key : string] : ApiParameter };
+}
+
+/**
+ * Represents an API parameter.
+ */
+export interface ApiParameter extends TypeDefinition {
+    name : string;
+    schema : {
+        $ref : string;
+    };
+    $ref : string;
+    customizationMetadata : { [key : string] : any };
+}
